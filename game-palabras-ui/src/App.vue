bb3317227913ebb07cbac5c9fbe1cb5d630c307c
juampi101820 <pablo101820@gmail.com>
2023-05-22 14:09:39 -0500

    dos

---
+++ game-palabras-ui/src/App.vue
@@ -0,0 +1,34 @@
+<!-- App.vue -->
+<template>
+  <div id="app">
+    <UsuarioComponent />
+    <JuegoComponent />
+  </div>
+</template>
+
+<script>
+import UsuarioComponent from './Usuario.vue';
+import JuegoComponent from './Juego.vue';
+
+export default {
+  name: 'App',
+  components: {
+    UsuarioComponent,
+    JuegoComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #b6b6b6;
+}
+
+body{
+  background-color: #181619;
+}
+</style>
